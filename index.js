c7c7827f190447e455b92264cb8b9effd8cb4f4a
stivex001 <stephenadeyemo@gmail.com>
2023-02-02 18:09:59 +0100

    settind up the folder structure

---
+++ index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+import {BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store, { persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  <Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
+     <App />
+  </PersistGate>
+   
+  </Provider>
+    
+  </BrowserRouter>
+);
+
