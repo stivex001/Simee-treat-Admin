e57b7bd4624fc32746f6334b47430eabaa00b7c0
stivex001 <stephenadeyemo@gmail.com>
2023-02-03 20:17:58 +0100

    feat: added links to sidebar components

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,27 +1,115 @@
+import { AttachMoney, ChatBubbleOutline, DynamicFeed, LineStyle, MailOutline, PermIdentity, Report, Storefront, Timeline, TrendingUp, WorkOutline } from "@mui/icons-material";
 import styled from "styled-components";
 
 const Container = styled.div`
-flex: 1;
+  flex: 1;
+  height: calc(100vh - 100px);
+  background-color: rgb(251, 251, 255);
+  position: sticky;
+  top: 50px;
 `;
-const Wrapper = styled.div``;
-const Menu = styled.div``;
-const Title = styled.div``;
+const Wrapper = styled.div`
+color: #555;
+padding: 20px;
+`;
+const Menu = styled.div`
+margin-bottom: 20px;
+`;
+const Title = styled.h3`
+font-size: 13px;
+  color: rgb(187, 186, 186);
+`;
+const List = styled.ul`
+list-style: none;
+`;
+const ListItem = styled.li`
+display: flex;
+align-items: center;
+`;
 
 const Sidebar = () => {
   return (
     <Container>
-        <Wrapper>
-    <Menu>
-    <Title>Dashboard</Title>
-    <Menu>
-        <li></li>
-        <li></li>
-        <li></li>
-    </Menu>
-    </Menu>
-    </Wrapper>
+      <Wrapper>
+        <Menu>
+          <Title>Dashboard</Title>
+          <List>
+            <ListItem>
+              <LineStyle style={{marginRight: '5px', fontSize: "20px"}} />
+              Home
+            </ListItem>
+            <ListItem>
+              <Timeline style={{marginRight: '5px', fontSize: "20px"}}/>
+              Analytics
+            </ListItem>
+            <ListItem>
+              <TrendingUp style={{marginRight: '5px', fontSize: "20px"}}/>
+              Sales
+            </ListItem>
+          </List>
+        </Menu>
+        <Menu>
+          <Title>Quick Menu</Title>
+          <List>
+            <ListItem>
+              <PermIdentity style={{marginRight: '5px', fontSize: "20px"}}/>
+              Users
+            </ListItem>
+            <ListItem>
+              <Storefront style={{marginRight: '5px', fontSize: "20px"}}/>
+              Products
+            </ListItem>
+            <ListItem>
+              <AttachMoney style={{marginRight: '5px', fontSize: "20px"}}/>
+              Transactions
+            </ListItem>
+            <ListItem>
+              <TrendingUp style={{marginRight: '5px', fontSize: "20px"}}/>
+              Report
+            </ListItem>
+          </List>
+        </Menu>
+        <Menu>
+          <Title>Notifications</Title>
+          <List>
+            <ListItem>
+              <MailOutline style={{marginRight: '5px', fontSize: "20px"}}/>
+              Mail
+            </ListItem>
+            <ListItem>
+              <DynamicFeed style={{marginRight: '5px', fontSize: "20px"}}/>
+              Feedback
+            </ListItem>
+            <ListItem>
+              <ChatBubbleOutline style={{marginRight: '5px', fontSize: "20px"}}/>
+              Messages
+            </ListItem>
+          </List>
+        </Menu>
+        <Menu>
+          <Title>Staff</Title>
+          <List>
+            <ListItem>
+              <WorkOutline style={{marginRight: '5px', fontSize: "20px"}}/>
+              Manage
+            </ListItem>
+            <ListItem>
+              <Timeline style={{marginRight: '5px', fontSize: "20px"}}/>
+              Analytics
+            </ListItem>
+            <ListItem>
+              <TrendingUp style={{marginRight: '5px', fontSize: "20px"}}/>
+              Transactions
+            </ListItem>
+            <ListItem>
+              <Report style={{marginRight: '5px', fontSize: "20px"}}/>
+              Reports
+            </ListItem>
+          </List>
+        </Menu>
+      </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
