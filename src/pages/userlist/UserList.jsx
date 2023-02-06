a2fbbca94af7f2a198cf3ab075e19dcc86b688b6
stivex001 <stephenadeyemo@gmail.com>
2023-02-06 17:59:03 +0100

    fix: typo fixed

--- src/pages/userlist/UserList.jsx
+++ src/pages/userlist/UserList.jsx
@@ -1,13 +1,84 @@
-import styled from "styled-components";
+import { DeleteOutline } from "@mui/icons-material";
+import { DataGrid } from "@mui/x-data-grid";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { userRows } from "../../components/chart/data";
+import { Button, Container, Image, Users } from "./userlist.styles";
 
-const Container = styled.div`
-flex: 4;
-margin-top: 10px;
-`;
+
 
 const UserList = () => {
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "user",
+      headerName: "User",
+      width: 150,
+      editable: true,
+      renderCell: (params) => {
+        return (
+          <Users>
+            <Image src={params.row.avatar} alt="user" />
+            {params.row.username}
+          </Users>
+        );
+      },
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 110,
+      editable: true,
+    },
+    {
+      field: "transaction",
+      headerName: "Transactions",
+      width: 160,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => (
+        <>
+          <Link to={"/user/" + params.row.id}>
+            <Button>Edit</Button>
+            </Link>
+            <DeleteOutline
+              style={{ color: "red", cursor: "pointer" }}
+              onClick={() => handleDelete(params.row.id)}
+            />
+
+        </>
+      ),
+    },
+  ];
+
   return (
-    <Container>UserList</Container>
+    <Container>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        pageSize={8}
+        rowsPerPageOptions={[8]}
+        checkboxSelection
+        disableSelectionOnClick
+        experimentalFeatures={{ newEditingApi: true }}
+      />
+    </Container>
   )
 }
 
---
+++ src/pages/userlist/userlist.styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+flex: 4;
+`;
+export const Users = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const Image = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-right: 10px;
+`;
+export const Button = styled.button`
+  border: none;
+  border-radius: 10px;
+  padding: 5px 10px;
+  background-color: #3bb077;
+  color: white;
+  cursor: pointer;
+  margin-right: 20px;
+`;
