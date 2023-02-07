79326eb497f1d065722a6aea173c3c86f1ae5400
stivex001 <stephenadeyemo@gmail.com>
2023-02-07 18:10:28 +0100

    feat: integrated redux into the project

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import './index.css';
 import App from './App';
 
 import {BrowserRouter} from 'react-router-dom'
-// import { Provider } from 'react-redux';
-// import store, { persistor } from './redux/store';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import store, { persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  {/* <Provider store={store}> */}
-  {/* <PersistGate loading={null} persistor={persistor}> */}
+  <Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
      <App />
-  {/* </PersistGate> */}
+  </PersistGate>
    
-  {/* // </Provider> */}
+   </Provider>
     
   </BrowserRouter>
 );
