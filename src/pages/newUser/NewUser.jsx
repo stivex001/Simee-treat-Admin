26c8dc59642a4929c549fc2bb7bbf284d933f7e8
stivex001 <stephenadeyemo@gmail.com>
2023-02-07 17:27:38 +0100

    feat: added styles to the new user components

--- src/pages/newUser/NewUser.jsx
+++ src/pages/newUser/NewUser.jsx
@@ -1,13 +1,74 @@
-import styled from "styled-components";
+import {
+  Button,
+  Container,
+  Form,
+  FormItems,
+  Gender,
+  GenderInput,
+  GenderLabel,
+  Input,
+  Label,
+  Option,
+  Select,
+  Title,
+} from "./newuser.styles";
 
-const Container = styled.div`
-flex: 4;
-`;
-
-const NewUser = () => {
+const Newuser = () => {
   return (
-    <Container>NewUser</Container>
-  )
-}
+    <Container>
+      <Title>New User</Title>
+      <Form>
+        <FormItems>
+          <Label>Username</Label>
+          <Input type="text" placeholder="baistevoo" />
+        </FormItems>
+        <FormItems>
+          <Label>Full Name</Label>
+          <Input type="text" placeholder="Adeyemo Stephen" />
+        </FormItems>
+        <FormItems>
+          <Label>Email</Label>
+          <Input type="email" placeholder="stephenadeyemo@gmail.com" />
+        </FormItems>
+        <FormItems>
+          <Label>Password</Label>
+          <Input type="password" placeholder="password" />
+        </FormItems>
+        <FormItems>
+          <Label>Phone</Label>
+          <Input type="text" placeholder="08162174754" />
+        </FormItems>
+        <FormItems>
+          <Label>Address</Label>
+          <Input type="text" placeholder="FCT | Nigeria" />
+        </FormItems>
+        <FormItems>
+          <Label>Gender</Label>
+          <Gender>
+            <GenderInput type="radio" name="gender" id="male" value="male" />
+            <GenderLabel for="male">Male</GenderLabel>
+            <GenderInput
+              type="radio"
+              name="gender"
+              id="female"
+              value="female"
+            />
+            <GenderLabel for="female">Female</GenderLabel>
+            <GenderInput type="radio" name="gender" id="other" value="other" />
+            <GenderLabel for="other">Other</GenderLabel>
+          </Gender>
+        </FormItems>
+        <FormItems>
+          <Label>Active</Label>
+          <Select name="active" id="active">
+            <Option value="yes">Yes</Option>
+            <Option value="no">No</Option>
+          </Select>
+        </FormItems>
+        <Button>Create</Button>
+      </Form>
+    </Container>
+  );
+};
 
-export default NewUser
+export default Newuser;
---
+++ src/pages/products/ProductList.jsx
@@ -0,0 +1,106 @@
+import styled from "styled-components";
+import { DataGrid } from "@mui/x-data-grid";
+import { DeleteOutline } from "@mui/icons-material";
+import { productRows } from "../../../components/AdminDashboard/chart/data";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+const Container = styled.div`
+  flex: 4;
+`;
+const Products = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const Image = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-right: 10px;
+`;
+const Button = styled.button`
+  border: none;
+  border-radius: 10px;
+  padding: 5px 10px;
+  background-color: #3bb077;
+  color: white;
+  cursor: pointer;
+  margin-right: 20px;
+`;
+
+const ProductList = () => {
+    const [data, setData] = useState(productRows)
+
+    const handleDelete = (id) => {
+        setData(data.filter((item) => item.id !== id));
+      };
+
+      const columns = [
+        { field: "id", headerName: "ID", width: 90 },
+        {
+          field: "product",
+          headerName: "Product",
+          width: 150,
+          editable: true,
+          renderCell: (params) => {
+            return (
+              <Products>
+                <Image src={params.row.img} alt="products" />
+                {params.row.name}
+              </Products>
+            );
+          },
+        },
+        {
+          field: "stock",
+          headerName: "Stock",
+          width: 150,
+          editable: true,
+        },
+        {
+          field: "status",
+          headerName: "Status",
+          width: 110,
+          editable: true,
+        },
+        {
+          field: "price",
+          headerName: "Price",
+          width: 160,
+        },
+        {
+          field: "action",
+          headerName: "Action",
+          width: 150,
+          renderCell: (params) => (
+            <>
+              <Link to={"/admin/product/" + params.row.id}>
+                <Button>ProductListEdit</Button>
+                </Link>
+                <DeleteOutline
+                  style={{ color: "red", cursor: "pointer" }}
+                  onClick={() => handleDelete(params.row.id)}
+                />
+    
+            </>
+          ),
+        },
+      ];
+    
+  return (
+    <Container>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        pageSize={8}
+        rowsPerPageOptions={[8]}
+        checkboxSelection
+        disableSelectionOnClick
+        experimentalFeatures={{ newEditingApi: true }}
+      />
+    </Container>
+  )
+}
+
+export default ProductList
