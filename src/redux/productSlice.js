9d6da8f4ee582e57868311d5c02a93d5153c99d9
stivex001 <stephenadeyemo@gmail.com>
2023-02-10 11:06:22 +0100

    feat:implemented product slice with redux toolkit

---
+++ src/redux/productSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const productSlice = createSlice({
+  name: "product",
+  initialState: {
+    products: [],
+    isFetching: false,
+    error: false,
+  },
+  reducers: {
+    // Get All Products
+    getProductStart: (state) => {
+        state.isFetching = true;
+        state.error = false;
+    },
+    getProductSuccess: (state, action) => {
+        state.isFetching = false;
+        state.products = action.payload;
+    },
+    getProductFailure: (state) => {
+        state.isFetching = false;
+        state.error = true;
+    }
+  },
+});
+
+export const { getProductStart, getProductSuccess, getProductFailure } = productSlice.actions;
+export default productSlice.reducer;
