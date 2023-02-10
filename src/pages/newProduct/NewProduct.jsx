a306232412f6b53b3425ed46c0dd6c06e4f622ad
stivex001 <stephenadeyemo@gmail.com>
2023-02-10 14:50:29 +0100

    feat:added firebase configuration

--- src/pages/newProduct/NewProduct.jsx
+++ src/pages/newProduct/NewProduct.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Button, Container, Form, FormItems, Input, Label, Option, Select, Title } from "./newProduct.styles"
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import app from "../../firebase";
 
 
 const NewProduct = () => {
@@ -19,6 +20,42 @@ const handleCat = (e) => {
 
 const handleClick = (e) => {
   e.preventDefault()
+  const fileName = new Date().getTime() + file.name
+  const storage = getStorage(app)
+  const storageRef = ref(storage, fileName) 
+  const uploadTask = uploadBytesResumable(storageRef, file);
+
+// Register three observers:
+// 1. 'state_changed' observer, called any time the state changes
+// 2. Error observer, called on failure
+// 3. Completion observer, called on successful completion
+uploadTask.on('state_changed', 
+  (snapshot) => {
+    // Observe state change events such as progress, pause, and resume
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case 'paused':
+        console.log('Upload is paused');
+        break;
+      case 'running':
+        console.log('Upload is running');
+        break;
+        default:
+    }
+  }, 
+  (error) => {
+    // Handle unsuccessful uploads
+  }, 
+  () => {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+      console.log('File available at', downloadURL);
+    });
+  }
+);
 
 }
 
