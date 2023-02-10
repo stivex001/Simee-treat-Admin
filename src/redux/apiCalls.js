d583c92100ee425aa09193c3af38217574c4e0ea
stivex001 <stephenadeyemo@gmail.com>
2023-02-11 00:11:35 +0100

    successfully populated the backend theough form

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,56 +1,65 @@
-
-import { publicRequest, userRequest } from "../requestMethods"
-import { addProductFailure, addProductStart, addProductSuccess, deleteProductFailure, deleteProductStart, deleteProductSuccess, getProductFailure, getProductStart, getProductSuccess, updateProductFailure, updateProductStart, updateProductSuccess } from "./productSlice"
-import { loginfailure, loginStart, loginSuccess } from "./userSlice"
-
+import { publicRequest, userRequest } from "../requestMethods";
+import {
+  addProductFailure,
+  addProductStart,
+  addProductSuccess,
+  deleteProductFailure,
+  deleteProductStart,
+  deleteProductSuccess,
+  getProductFailure,
+  getProductStart,
+  getProductSuccess,
+  updateProductFailure,
+  updateProductStart,
+  updateProductSuccess,
+} from "./productSlice";
+import { loginfailure, loginStart, loginSuccess } from "./userSlice";
 
 export const login = async (dispatch, user) => {
-
-    dispatch(loginStart())
-    try {
-       const res = await publicRequest.post("/users/login", user)
-       dispatch(loginSuccess(res.data))
-    } catch (error) {
-        dispatch(loginfailure())
-    }
-}
+  dispatch(loginStart());
+  try {
+    const res = await publicRequest.post("/users/login", user);
+    dispatch(loginSuccess(res.data));
+  } catch (error) {
+    dispatch(loginfailure());
+  }
+};
 
 export const getProducts = async (dispatch) => {
-    dispatch(getProductStart())
-    try {
-        const res = await publicRequest.get('/product')
-        dispatch(getProductSuccess(res.data))
-    } catch (error) {
-        dispatch(getProductFailure())
-    }
-}
+  dispatch(getProductStart());
+  try {
+    const res = await publicRequest.get("/product");
+    dispatch(getProductSuccess(res.data));
+  } catch (error) {
+    dispatch(getProductFailure());
+  }
+};
 
 export const deleteProduct = async (id, dispatch) => {
-    dispatch(deleteProductStart())
-    try {
-        const res = await userRequest.delete(`/product/${id}`)
-        dispatch(deleteProductSuccess(res.data))
-    } catch (error) {
-        dispatch(deleteProductFailure())
-    }
-}
+  dispatch(deleteProductStart());
+  try {
+    const res = await userRequest.delete(`/product/${id}`);
+    dispatch(deleteProductSuccess(res.data));
+  } catch (error) {
+    dispatch(deleteProductFailure());
+  }
+};
 
 export const updateProduct = async (id, product, dispatch) => {
-    dispatch(updateProductStart())
-    try {
-        // const res = await userRequest.delete(`/product/${id}`)
-        dispatch(updateProductSuccess({id, product}))
-    } catch (error) {
-        dispatch(updateProductFailure())
-    }
-}
+  dispatch(updateProductStart());
+  try {
+    dispatch(updateProductSuccess({ id, product }));
+  } catch (error) {
+    dispatch(updateProductFailure());
+  }
+};
 
 export const addProduct = async (product, dispatch) => {
-    dispatch(addProductStart())
-    try {
-        const res = await userRequest.post(`/product`, {product})
-        dispatch(addProductSuccess(res.data))
-    } catch (error) {
-        dispatch(addProductFailure())
-    }
-}
+  dispatch(addProductStart());
+  try {
+    const res = await userRequest.post(`/product`, product);
+    dispatch(addProductSuccess(res.data));
+  } catch (error) {
+    dispatch(addProductFailure());
+  }
+};
--- src/redux/productSlice.js
+++ src/redux/productSlice.js
@@ -21,53 +21,53 @@ const productSlice = createSlice({
       state.isFetching = false;
       state.error = true;
     },
-  },
 
-  // Delete Product
-  deleteProductStart: (state) => {
-    state.isFetching = true;
-    state.error = false;
-  },
-  deleteProductSuccess: (state, action) => {
-    state.isFetching = false;
-    state.products.splice(
-      state.products.findIndex((item) => item._id === action.payload),
-      1
-    );
-  },
-  deleteProductFailure: (state) => {
-    state.isFetching = false;
-    state.error = true;
-  },
+    // Delete Product
+    deleteProductStart: (state) => {
+      state.isFetching = true;
+      state.error = false;
+    },
+    deleteProductSuccess: (state, action) => {
+      state.isFetching = false;
+      state.products.splice(
+        state.products.findIndex((item) => item._id === action.payload),
+        1
+      );
+    },
+    deleteProductFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
 
-  // Update Product
-  updateProductStart: (state) => {
-    state.isFetching = true;
-    state.error = false;
-  },
-  updateProductSuccess: (state, action) => {
-    state.isFetching = false;
-    state.products[
-      state.products.findIndex((item) => item._id === action.payload)
-    ] = action.payload.product;
-  },
-  updateProductFailure: (state) => {
-    state.isFetching = false;
-    state.error = true;
-  },
+    // Update Product
+    updateProductStart: (state) => {
+      state.isFetching = true;
+      state.error = false;
+    },
+    updateProductSuccess: (state, action) => {
+      state.isFetching = false;
+      state.products[
+        state.products.findIndex((item) => item._id === action.payload)
+      ] = action.payload.product;
+    },
+    updateProductFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
 
-  // Add Product
-  addProductStart: (state) => {
-    state.isFetching = true;
-    state.error = false;
-  },
-  addProductSuccess: (state, action) => {
-    state.isFetching = false;
-    state.products.push(action.payload);
-  },
-  addProductFailure: (state) => {
-    state.isFetching = false;
-    state.error = true;
+    // Add Product
+    addProductStart: (state) => {
+      state.isFetching = true;
+      state.error = false;
+    },
+    addProductSuccess: (state, action) => {
+      state.isFetching = false;
+      state.products.push(action.payload);
+    },
+    addProductFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
   },
 });
 
